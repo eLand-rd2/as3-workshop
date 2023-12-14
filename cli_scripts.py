ba1333144e42aea4df31d304967d8bdf7a2acff4
victoria304040 <152965405+victoria304040@users.noreply.github.com>
2023-12-14 14:58:38 +0800

    新增一個子命令，利用@click.group()將子命令新增進去

--- cli_scripts.py
+++ cli_scripts.py
@@ -14,3 +14,26 @@ def say_hello(count, name):
 def print_time(timezone):
     # TODO print current time
     pass
+
+
+@click.command()
+@click.option('--height', prompt='Your height (in meters)', type=float, help='Number of height.')
+@click.option('--weight', prompt='Your weight (in kg)', type=float, help='Number of weight.')
+def calculate_bmi(height, weight):
+    bmi = weight/(height**2)
+    click.echo(f"您的bmi為 {bmi:.2f}!")
+
+
+# Create Click command group
+@click.group()
+def cli():
+    """A simple command-line tool."""
+    pass
+
+
+# Add commands to the group
+cli.add_command(say_hello)
+cli.add_command(calculate_bmi)
+
+if __name__ == '__main__':
+    cli()
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-from cli_scripts import say_hello
+import click
+from cli_scripts import cli  # Assuming your script is named cli_script.py
+
 if __name__ == '__main__':
-    say_hello()
+    cli()
+
