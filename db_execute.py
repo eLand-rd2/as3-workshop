dbc858cd27d35b3b3ced968f4143bf4f6fd1dfba
weifan <p988744@gmail.com>
2023-12-19 16:10:48 +0800

    add 資料庫部分的程式碼

---
+++ db_execute.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+# 建立與資料庫的連接（如果不存在則會被創建），'example.db'是資料庫文件的名稱
+conn = sqlite3.connect('example.db')
+
+# 創建一個游標對象cursor → 可以用來存取每一個對資料庫的查詢/處理後的紀錄，且後續可以針對cursor進行各種操作，譬如修改數據、檢查數據
+cursor = conn.cursor()
+
+# 建立一個名為"users"的資料表
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS articles (
+        id INTEGER PRIMARY KEY, 
+        title TEXT, 
+        content TEXT,
+        url TEXT)''')
+
+# 插入一筆資料
+# 這裡使用了參數化查詢，以防止SQL注入攻擊。
+cursor.execute("INSERT INTO articles (title, content, url) VALUES (?, ?, ?)", ('Article Title', 'Article Content', 'Article URL'))
+
+
+# 查詢資料
+cursor.execute("SELECT * FROM articles")
+print(cursor.fetchall())  # fetchall() 獲取查詢結果的所有資料，並輸出成列表形式。
+
+# 提交變更並關閉連接
+conn.commit()
+conn.close()
