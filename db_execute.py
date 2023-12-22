2539e28f5651ea03364859a3f201b776e5f6bf12
victoria304040 <152965405+victoria304040@users.noreply.github.com>
2023-12-22 15:45:16 +0800

    add 資料庫部分的程式碼

--- db_execute.py
+++ db_execute.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 # 建立與資料庫的連接（如果不存在則會被創建），'example.db'是資料庫文件的名稱
-conn = sqlite3.connect('example.db')
+conn = sqlite3.connect('AS3_data.db')
 
 # 創建一個游標對象cursor → 可以用來存取每一個對資料庫的查詢/處理後的紀錄，且後續可以針對cursor進行各種操作，譬如修改數據、檢查數據
 cursor = conn.cursor()
