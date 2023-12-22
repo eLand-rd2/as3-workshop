701723f89b9a73cd20db73cc75f125254bcd38bf
victoria304040 <152965405+victoria304040@users.noreply.github.com>
2023-12-22 15:41:12 +0800

    add 資料庫部分的程式碼

--- db_execute.py
+++ db_execute.py
@@ -6,7 +6,7 @@ conn = sqlite3.connect('example.db')
 # 創建一個游標對象cursor → 可以用來存取每一個對資料庫的查詢/處理後的紀錄，且後續可以針對cursor進行各種操作，譬如修改數據、檢查數據
 cursor = conn.cursor()
 
-# 建立一個名為"users"的資料表
+# 建立一個資料表
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS articles (
         id INTEGER PRIMARY KEY, 
@@ -15,7 +15,7 @@ cursor.execute('''
         url TEXT)''')
 
 # 插入一筆資料
-# 這裡使用了參數化查詢，以防止SQL注入攻擊。
+# 這裡使用了參數化查詢，以防止SQL注入攻擊。→GPT給的參考但我沒有很懂這個
 cursor.execute("INSERT INTO articles (title, content, url) VALUES (?, ?, ?)", ('Article Title', 'Article Content', 'Article URL'))
 
 
@@ -26,3 +26,5 @@ print(cursor.fetchall())  # fetchall() 獲取查詢結果的所有資料，並�
 # 提交變更並關閉連接
 conn.commit()
 conn.close()
+
+#我不太確定這個要在哪邊加上if __name__ == '__main__': 這段QQ
