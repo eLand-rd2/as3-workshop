5ca9d70b70be69b599e98b97a83e6e1bc79619a8
kkrislai <153599720+kkrislai@users.noreply.github.com>
2024-01-02 17:15:52 +0800

    add隨機useragent

--- spiders/base.py
+++ spiders/base.py
@@ -8,15 +8,16 @@ class BaseSpider:
         self.ua = UserAgent()  #每次查詢初始化 UserAgent
 
     def request_page(self, url, headers=None, cookies=None):
-            response = requests.get(url, headers=headers, cookies=cookies)
-            headers = {'user-agent': self.ua.random
-                       }
-            if response.status_code == 200:
-                return response.text
-            else:
-                # print error message
-                print(f"Error: {response.status_code}, {response.text}")
-                return None
+        for url in settings.base_url :
+                response = requests.get(url, headers=headers, cookies=cookies)
+                headers = {'user-agent': self.ua.random
+                           }
+                if response.status_code == 200:
+                    return response.text
+                else:
+                    # print error message
+                    print(f"Error: {response.status_code}, {response.text}")
+                    return None
 
     def parse_page(self, response):
         soup = BeautifulSoup(response, 'html.parser')
