405c7a302eadfe7702d780e1a1889218bf8923d9
victoria304040 <152965405+victoria304040@users.noreply.github.com>
2024-01-25 16:06:17 +0800

    發現蝦皮跟momo都有品牌對應的code value，想將其存進資料庫內，故在Schemas\Brand的資料表格式中加上value

--- schemas/brand.py
+++ schemas/brand.py
@@ -5,14 +5,18 @@ import dataclasses
 class BrandRead:
     id: int
     name: str
+    # 如果我想要把momo跟蝦皮上對應品牌的code value 加入資料表中一起存
+    # value: str
 
 
 @dataclasses.dataclass
 class BrandCreate:
     name: str
+    # value: str
 
 
 @dataclasses.dataclass
 class BrandUpdate:
     id: int
     name: str
+    # value: str
