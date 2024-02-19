98cdd2ae62e5c152586af2d76a7d82f8a461c6d0
kkrislai <153599720+kkrislai@users.noreply.github.com>
2024-02-19 16:34:27 +0800

    text 指定為字串

--- spiders/run_shopee.py
+++ spiders/run_shopee.py
@@ -1,12 +1,16 @@
 from spiders.shopee_spider import ShopeeSpider
 
+result =[]
+
 spider = ShopeeSpider()
 shops = ['56678703']
 # , '779422436', '37004578', '56678703', '70001183', '37008598', '747940835', '774925409'
 
 for shop in shops:
     for n in range(0, 901, 100) :
-        url = f'https://shopee.tw/api/v4/seller_operation/get_shop_ratings_new?limit=30&offset={n}&shopid={shop}&userid=779508643'
+        print(f'{shop=}, {n=}, {len(result)}')
+        url = f'https://shopee.tw/api/v4/seller_operation/get_shop_ratings_new?limit=100&offset={n}&shopid={shop}&userid=779508643'
         response = spider.request_page(url, headers=None, cookies=None)
         payload = spider.parse_page(response)
+        result.extend(payload)
         spider.save_data(payload)
